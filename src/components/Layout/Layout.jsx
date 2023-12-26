a80c79f4122404e70bb1e9c0d5e20d4ddfeab645
Illia Kovalov <kovalev.illia2009@gmail.com>
2023-12-26 19:59:01 +0100

    171717

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,55 +1,13 @@
-/*import { LuContact2 } from "react-icons/lu";
-
+import React from 'react';
+import { LuContact2 } from 'react-icons/lu';
 import { Container, Nav, SNavLink, LogoWrapper, LinkWrapper, WrapperItem } from './Layout.styled.js';
 import css from './Layout.module.css';
-
 import InfoUser from '../InfoUser/InfoUser';
-
-const Layout = () => {
-  return (
-    <>
-      <Nav className={css.navigation}>
-        <Container className={css.container}>
-          <LogoWrapper>
-            <LuContact2 className={css.icon} />
-            PhoneBook
-          </LogoWrapper>
-          <LinkWrapper>
-            <WrapperItem>
-              <SNavLink to="/signup">
-                Sign Up
-              </SNavLink>
-            </WrapperItem>
-            <WrapperItem>
-              <SNavLink to="/login" className={css.link}>
-                Login
-              </SNavLink>
-            </WrapperItem>
-          </LinkWrapper>
-          <InfoUser className="d-flex flex-column" />
-        </Container>
-      </Nav>
-    </>
-  );
-};
-
-export default Layout;*/
-
-
-
-import { LuContact2 } from "react-icons/lu";
-import { useLocation } from 'react-router-dom';  
-
-import { Container, Nav, SNavLink, LogoWrapper, LinkWrapper, WrapperItem } from './Layout.styled.js';
-import css from './Layout.module.css';
-
-import InfoUser from '../InfoUser/InfoUser';
+import { useLocation } from 'react-router-dom';
 
 const Layout = () => {
   const location = useLocation();
-
   const isContactsPage = location.pathname === '/contacts';
-  const isNotContactsPage = location.pathname === '/signup' || location.pathname === '/login';
 
   return (
     <>
@@ -60,26 +18,18 @@ const Layout = () => {
             PhoneBook
           </LogoWrapper>
           <LinkWrapper>
-            {!isContactsPage && ( 
+            {!isContactsPage && (
               <>
                 <WrapperItem>
-                  <SNavLink to="/signup">
-                    Sign Up
-                  </SNavLink>
+                  <SNavLink to="/signup">Sign Up</SNavLink>
                 </WrapperItem>
                 <WrapperItem>
-                  <SNavLink to="/login" className={css.link}>
-                    Login
-                  </SNavLink>
+                  <SNavLink to="/login" className={css.link}>Login</SNavLink>
                 </WrapperItem>
-              </>
-            )}
-            {!isNotContactsPage && (
-              <>
-                <InfoUser className="d-flex flex-column" />
               </>
             )}
           </LinkWrapper>
+          {isContactsPage && <InfoUser className="d-flex flex-column" />}
         </Container>
       </Nav>
     </>
