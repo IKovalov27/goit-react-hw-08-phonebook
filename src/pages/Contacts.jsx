87677fad6c7c362af6369d2a1345e85dddb01b0c
Illia Kovalov <kovalev.illia2009@gmail.com>
2024-01-03 23:40:01 +0100

    171717

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,3 +1,6 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getContactsThunk } from 'redux/contactsThunk';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { Filter } from 'components/Filter/Filter';
 import { ContactList } from 'components/ContactList/ContactList';
@@ -5,26 +8,24 @@ import { ContactList } from 'components/ContactList/ContactList';
 import { Title } from './Contacts.styled';
 import css from '../components/App.module.css';
 
-const { useEffect } = require('react');
-const { useDispatch, useSelector } = require('react-redux');
-const { getContactsThunk } = require('redux/contactsThunk');
-
 const Contacts = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(state => state.contacts.items);
   const filtered = useSelector(state => state.filter);
-  const filterContact = e => {
-    const filteredContacts = contacts.filter(contact =>
+
+  const filterContact = () => {
+    return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filtered.toLowerCase())
     );
-    return filteredContacts;
   };
+
   useEffect(() => {
-    dispatch(getContactsThunk);
+    dispatch(getContactsThunk());
   }, [dispatch]);
+
   return (
     <div className={css.container}>
-      <ContactForm/>
+      <ContactForm />
       <div>
         <Title>Contacts</Title>
         <Filter />
